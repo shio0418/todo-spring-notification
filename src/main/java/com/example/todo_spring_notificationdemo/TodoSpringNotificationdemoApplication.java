3aaab950f13ad8b0366c4e523b8368e8c4ae2465
古宮詩桜 <furumiya.s.aa@m.titech.ac.jp>
2026-06-14 14:20:34 +0900

    feat: springのプロジェクトファイルを追加

---
+++ src/main/java/com/example/todo_spring_notificationdemo/TodoSpringNotificationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_spring_notificationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringNotificationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSpringNotificationdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todo_spring_notificationdemo/TodoSpringNotificationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_spring_notificationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringNotificationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
