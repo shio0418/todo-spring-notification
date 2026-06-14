cb996f6d0d0c94d0cd0f1e3e7109e604d292a36a
古宮詩桜 <furumiya.s.aa@m.titech.ac.jp>
2026-06-14 14:22:54 +0900

    chore: ファイル名変更

--- src/main/java/com/example/todo_spring_notificationdemo/TodoSpringNotificationApplication.java
+++ src/main/java/com/example/todo_spring_notificationdemo/TodoSpringNotificationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoSpringNotificationdemoApplication {
+public class TodoSpringNotificationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoSpringNotificationdemoApplication.class, args);
+		SpringApplication.run(TodoSpringNotificationApplication.class, args);
 	}
 
 }
